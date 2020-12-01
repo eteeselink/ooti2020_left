ab945b9c5789165cfe9ede1d05fd892ba05b9bde
Nyambuu <l.nyambuu@tue.nl>
2020-12-01 15:00:12 +0100

    firstAllVotes

---
+++ Kiosk/AllVotes.cs
@@ -0,0 +1,27 @@
+using Spectre.Console;
+using System.Collections.Generic;
+
+namespace Kiosk
+{
+    class AllVotesSubmit
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> raw_answers = new Dictionary<int, string>();
+
+            var survey = new Survey();
+            var question = survey.GetQuestion();
+
+            var prompt = new TextPrompt<string>(question.Question);
+            foreach(var a in question.Answers) {
+                prompt.AddChoice(a);
+            }
+            var answer = AnsiConsole.Prompt(prompt);
+
+            raw_answers.Add(1, answer);
+
+            
+    
+        }
+    }
+}
