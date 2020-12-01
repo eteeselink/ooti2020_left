c874826044238c4b6bbb9fd82f69fd1d1377774c
abolfaz.saravani70 <abolfaz.saravani70@gmail.com>
2020-12-01 15:57:18 +0100

    added asking the user for file path

--- Kiosk/Program.cs
+++ Kiosk/Program.cs
@@ -11,7 +11,9 @@ namespace Kiosk
     {
         static void Main(string[] args)
         {
-            InputReader inputReader = new InputReader(@"Kiosk/question.json");
+            string filePath=AnsiConsole.Ask<string>("Please Enter the file path.");
+    
+            InputReader inputReader = new InputReader(filePath);
             
             // var survey = new Survey();
             // var question = survey.GetQuestion();
@@ -30,6 +32,7 @@ namespace Kiosk
             // else {
             //     AnsiConsole.Markup("That's [underline red]wrong[/]!");
             // }
+
         }
     }
 }
