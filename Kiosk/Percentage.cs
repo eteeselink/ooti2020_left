2ee1c8b3a3499b57eb119a315e00aee5a420896e
Kaplan <h.kaplan@tue.nl>
2020-12-01 15:41:37 +0100

    adding class

---
+++ Kiosk/Percentage.cs
@@ -0,0 +1,26 @@
+using Spectre.Console;
+using System;
+namespace Kiosk {
+
+    class Percentage {
+
+        int yesCounter, noCounter = 0;
+        double yesPercentage, noPercentage;
+
+        // input = Text file input
+        // choice = input.ToLower();
+        // if (choice == "yes")
+        // {
+        //     yesCounter++;
+        // }
+        // else if (choice == "no")
+        // {
+        //     noCounter++;
+        // };
+
+        // yesPercentage = yesCounter / (yesCounter + noCounter) * 100;
+        // noPercentage = 100 - yesPercentage;
+        
+        // Console.Write("The percentage of yes is: {0}%", yesPercentage);
+        }
+}
