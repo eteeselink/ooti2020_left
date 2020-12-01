25de312540f15a10d080f8bfa90b7e9bc82567ec
Putra <k.a.r.putra@tue.nl>
2020-12-01 15:26:59 +0100

    getQuestion method

--- Kiosk/AllVotes.cs
+++ Kiosk/AllVotes.cs
@@ -1,27 +1,27 @@
-using Spectre.Console;
-using System.Collections.Generic;
+// using Spectre.Console;
+// using System.Collections.Generic;
 
-namespace Kiosk
-{
-    class AllVotesSubmit
-    {
-        static void Main(string[] args)
-        {
-            Dictionary<int, string> raw_answers = new Dictionary<int, string>();
+// namespace Kiosk
+// {
+//     class AllVotesSubmit
+//     {
+//         static void Main(string[] args)
+//         {
+//             Dictionary<int, string> raw_answers = new Dictionary<int, string>();
 
-            var survey = new Survey();
-            var question = survey.GetQuestion();
+//             var survey = new Survey();
+//             var question = survey.GetQuestion();
 
-            var prompt = new TextPrompt<string>(question.Question);
-            foreach(var a in question.Answers) {
-                prompt.AddChoice(a);
-            }
-            var answer = AnsiConsole.Prompt(prompt);
+//             var prompt = new TextPrompt<string>(question.Question);
+//             foreach(var a in question.Answers) {
+//                 prompt.AddChoice(a);
+//             }
+//             var answer = AnsiConsole.Prompt(prompt);
 
-            raw_answers.Add(1, answer);
+//             raw_answers.Add(1, answer);
 
             
     
-        }
-    }
-}
+//         }
+//     }
+// }
--- Kiosk/InputReader.cs
+++ Kiosk/InputReader.cs
@@ -10,19 +10,38 @@ using Spectre.Console;
 namespace Kiosk{
     public class InputReader{
 
-        private Collection<Input> questions = new Collection<Input>();
+        // private Collection<Input> questions = new Collection<Input>();
 
         public InputReader(string path){
-            // string rawText = System.IO.File.ReadAllText(path);
+            string rawText = System.IO.File.ReadAllText(path);
             string jsonString = File.ReadAllText(path);
-            JsonTextReader reader = new JsonTextReader(new StringReader (jsonString));
 
-            while(reader.Read()){
-                AnsiConsole.Markup(reader.TokenType.ToString());
+            AnsiConsole.Markup(jsonString);
+            // JsonTextReader reader = new JsonTextReader(new StringReader (jsonString));
+
+            // while(reader.Read()){
+            //     AnsiConsole.Markup(reader.TokenType.ToString());
                 //populate the content of the file
                 //stil working on it
-            }
+            // }
     }
+
+    public struct MultipleChoiceQuestion {
+        public string Question { get; init; }
+        public string[] Answers { get; init; }
+    }
+
+    public MultipleChoiceQuestion GetQuestion() {
+            return new MultipleChoiceQuestion {
+                Question = "What's your favorite color?",
+                Answers = new[] { "Red", "Blue", "Green"}
+            };
+        }
+
+        public string GetCorrectAnswer() {
+            return "Blue";
+        }
+
     }
 
 }
