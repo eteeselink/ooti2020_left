f5441e7a9e879f1310496f75a227adfd54bd5a32
Putra <k.a.r.putra@tue.nl>
2020-12-01 16:05:56 +0100

    refactoring Question.cs

--- Kiosk/InputReader.cs
+++ Kiosk/InputReader.cs
@@ -10,19 +10,22 @@ using Spectre.Console;
 namespace Kiosk{
     public class InputReader{
 
-        // private Collection<Input> questions = new Collection<Input>();
+        private Collection<Question> questions;
+         JsonTextReader reader;
 
         public InputReader(string path){
             string rawText = System.IO.File.ReadAllText(path);
-            //AnsiConsole.Markup(rawText);
-            JsonTextReader reader = new JsonTextReader(new StringReader(rawText));
-            
-            while(reader.Read()){
-                AnsiConsole.Markup("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
-                // populate the content of the file
-                // stil working on it
-            }
+            reader = new JsonTextReader(new StringReader(rawText));
+            questions =  new Collection<Question>();
     }
+
+    public void readFile(){
+        // while(reader.Read()){
+            
+        //     questions.Add(new Question(reader.TokenType.ToString, ));
+        // }
+    }
+
 
     public struct MultipleChoiceQuestion {
         public string Question { get; init; }
@@ -30,15 +33,15 @@ namespace Kiosk{
     }
 
     public MultipleChoiceQuestion GetQuestion() {
-            return new MultipleChoiceQuestion {
-                Question = "What's your favorite color?",
-                Answers = new[] { "Red", "Blue", "Green"}
-            };
-        }
+        return new MultipleChoiceQuestion {
+            Question = "What's your favorite color?",
+            Answers = new[] { "Red", "Blue", "Green"}
+        };
+    }
 
-        public string GetCorrectAnswer() {
-            return "Blue";
-        }
+    public string GetCorrectAnswer() {
+        return "Blue";
+    }
 
     }
 
